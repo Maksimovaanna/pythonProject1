3fb11c5fca75317125decbd21958a4dcd484a2f5
melan <pls@pls>
2023-01-26 16:19:02 +0300

    work

--- mysite/proj/analyze.py
+++ mysite/proj/analyze.py
@@ -1,10 +1,10 @@
 import os
 import json
-pylint = os.popen('pylint ./app').read()
+pylint = os.popen('pylint ./views.py').read()
 if pylint:
      print('Ошибки качества кода')
 
-os.system('bandit -iii -lll -q -r ./app/ -o bandit.json -f json ')
+os.system('bandit -iii -lll -q -r ./views.py -o bandit.json -f json ')
 with open('bandit.json', 'r') as f:
     b = json.load(f)
 if b['results']:
